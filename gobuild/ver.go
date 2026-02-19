9b967c1758caa7515d4ae1046411c7d3fa19fd99
zichou <1303037989@qq.com>
2026-02-19 21:12:04 +0800

    feat: 增加 darwin 平台

--- gobuild/ver.go
+++ gobuild/ver.go
@@ -8,6 +8,8 @@ type Versions struct {
 var Ver = []Versions{
 	{GOOS: "linux", GOARCH: "amd64"},
 	{GOOS: "linux", GOARCH: "arm64"},
+	{GOOS: "darwin", GOARCH: "arm64"},
+	{GOOS: "darwin", GOARCH: "amd64"},
 	{GOOS: "android", GOARCH: "arm64"},
 	{GOOS: "windows", GOARCH: "amd64"},
 }
